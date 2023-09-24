bdc6988a6a4f3e3cff4c665a364f248e82c3adad
alexdr3437 <alexanderdingwall5398@gmail.com>
2023-09-24 19:54:21 -0400

    format + pyright

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,11 +2,12 @@ local lsp_zero = require('lsp-zero')
 
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({buffer = bufnr})
-
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
+
+require'lspconfig'.pyright.setup{}
 
 require('lspconfig').lua_ls.setup({
     settings = {
@@ -17,5 +18,5 @@ require('lspconfig').lua_ls.setup({
         }
     }
 })
-require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.clangd.setup{}
+require 'lspconfig'.rust_analyzer.setup {}
+require 'lspconfig'.clangd.setup {}
