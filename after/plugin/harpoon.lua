842e97e2e76e89222425ea20552e7f4dcf5a062d
alexdr3437 <alexanderdingwall5398@gmail.com>
2023-09-27 20:54:17 -0400

    harpoon remaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,8 +4,13 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<C-r>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-y>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-u>", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>hf", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>hg", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>hh", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>hj", function() ui.nav_file(4) end)
 
+vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end)
+vim.keymap.set("n", "<leader>hp", function() ui.nav_prev() end)
+
+local term = require("harpoon.term")
+vim.keymap.set("n", "<leader>ht", function() term.gotoTerminal(1) end)
