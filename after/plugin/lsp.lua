c1a065da1017b139ae80cd85c1fbcd5075ce97c0
alexdr3437 <alexanderdingwall5398@gmail.com>
2023-11-19 13:08:47 -0500

    change search to live_grep

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,15 +1,13 @@
 local lsp_zero = require('lsp-zero')
 
-
 lsp_zero.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
     lsp_zero.default_keymaps({ buffer = bufnr })
     vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end)
-
 end)
 
-require'lspconfig'.pyright.setup{}
+require 'lspconfig'.pyright.setup {}
 
 require('lspconfig').lua_ls.setup({
     settings = {
@@ -20,6 +18,7 @@ require('lspconfig').lua_ls.setup({
         }
     }
 })
+
 require 'lspconfig'.rust_analyzer.setup {}
 require 'lspconfig'.clangd.setup {}
 
