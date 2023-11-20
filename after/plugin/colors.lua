8122647701f14511d735f570e1514706b8b2ca2a
alexdr3437 <alexanderdingwall5398@gmail.com>
2023-11-20 08:25:49 -0500

    translucent bg

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,5 +1,8 @@
 function ColorMyPencils()
 	vim.cmd.colorscheme('catppuccin')
+
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 
 ColorMyPencils()
